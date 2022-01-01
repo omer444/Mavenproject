8b826f55ae8579c6395c1c72b9290faaf8b2cc67
omer444 <ommerfar4@gmail.com>
2022-01-01 04:35:55 +0000

    added more hello feature

--- src/main/java/hello.java
+++ src/main/java/hello.java
@@ -4,6 +4,8 @@ public class hello {
 
         System.out.println("hello");
         System.out.println("developing hello feature");
+        System.out.println("local repo is already published to github");
+        System.out.println("doing more work");
 
     }
 
